849f9abdc13c77f3d7b83c114d84d3285b3d6d12
Thibault LEPEZ <thibault.lepez@efrei.net>
2020-05-31 14:16:25 +0200

    Change app general look

---
+++ src/components/Appbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app-bar
+      app
+      color="primary"
+      dark
+      clipped-left
+    >
+
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-navigation-drawer
+      color="info"
+      dark
+      app
+      clipped
+      permanent
+    >
+      <v-list>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+          :to="item.to"
+        >
+          <v-list-item-icon>
+            <i :class="item.icon"></i>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <template v-slot:append>
+        <div class="pa-2">
+          <v-btn
+            :color="isLoggedIn ? 'secondary' : 'accent'"
+            @click="actionBtn"
+            block
+          >{{ isLoggedIn ? 'Logout' : 'Login' }}</v-btn>
+        </div>
+      </template>
+    </v-navigation-drawer>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'Navbar',
+  data: () => ({
+    items: [
+      { title: 'Callendrier', icon: 'gg-calendar', to: '/' },
+      { title: 'Ajouter une séance', icon: 'gg-add', to: '/add-session' },
+      { title: 'Ajouter un patient', icon: 'gg-user-add', to: '/add-patient' },
+      { title: 'Liste de patient', icon: 'gg-user-list', to: '/patient' },
+    ],
+  }),
+
+  methods: {
+    actionBtn() {
+      if (this.isLoggedIn) this.$store.dispatch('auth/logout');
+      else this.$router.push('/login');
+    },
+  },
+
+  computed: {
+    ...mapGetters({
+      isLoggedIn: 'auth/isLoggedIn',
+    }),
+  },
+};
+</script>
+
+<style>
+
+</style>
